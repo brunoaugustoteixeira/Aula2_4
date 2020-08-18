4becaa59f39d0e045073f82de664f71f488173d4
bruno.teixeira <brunoaugustoteixeira@igti.com.br>
2020-08-17 23:24:05 -0300

    Adiciona rota PUT

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -52,4 +52,18 @@ app.patch('/student/:id', async (req, res) => {
   }
 });
 
+app.put('/student/:id', async (req, res) => {
+  try {
+    const student = await studentModel.findOneAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    res.send(student);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
 export { app as studentRouter };
